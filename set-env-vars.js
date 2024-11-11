2009b6464384b0d58f0b967ab5dc5e62f1dbed18
temic137 <68796483+temic137@users.noreply.github.com>
2024-11-11 19:06:25 +0100

    deployment fix

--- set-env-vars.js
+++ set-env-vars.js
@@ -1,28 +1,22 @@
+require('dotenv').config();  // Add this line to load variables from the .env file
 const fs = require('fs');
 const path = require('path');
 
-// Get the GitHub token from the environment variables
 const githubToken = process.env.GITHUB_TOKEN;
 
-// Check if the GITHUB_TOKEN is provided
 if (!githubToken) {
   console.error('GITHUB_TOKEN is not set!');
-  process.exit(1);  // Exit with an error if the token is not found
+  process.exit(1);
 }
 
-// Define the path to the environment.prod.ts file
-const envFilePath = path.resolve(__dirname, 'src/environments/environment.prod.ts');
-
-// Read the content of environment.prod.ts
+const envFilePath = path.resolve(__dirname, 'src/environments/environments.prod.ts');
 let envFileContent = fs.readFileSync(envFilePath, 'utf8');
 
-// Replace the placeholder with the real GitHub token
 envFileContent = envFileContent.replace(
-  /process\.env\.GITHUB_TOKEN/g,  // This is the placeholder in your environment.prod.ts
-  `'${githubToken}'`  // Replace with the actual GitHub token
+  /process\.env\.GITHUB_TOKEN/g,
+  `'${githubToken}'`
 );
 
-// Write the updated content back to the environment.prod.ts file
 fs.writeFileSync(envFilePath, envFileContent, 'utf8');
 
 console.log('Environment variables set successfully!');
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
-import { environment } from './environments/environments';
+import { environment } from '../environments/environments';
 @Injectable({
   providedIn: 'root'
 })
